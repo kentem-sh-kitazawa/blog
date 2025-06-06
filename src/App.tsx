121961590559abd96bbc9cb20fd3852c62ec5cec
sh-kitazawa <sh-kitazawa@kentem.co.jp>
2025-06-06 15:35:22 +0900

    importでファイル形式を指定・propsで更新関数を渡すように変更

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "./components/Home";
 import CreatePost from "./components/CreatePost";
 import Login from "./components/Login";
-import Logout from "./components/Logout";
+import Logout from "./components/Logout.tsx";
 import Navbar from "./components/Navbar";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [isAuth, setisAuth] = useState<any>(false);
   return (
     <Router>
       <Navbar />
@@ -13,8 +15,11 @@ function App() {
         {/* URLの最後にpathを付け足すと行きたいコンポーネントを指定できる*/}
         <Route path="/" element={<Home />}></Route>
         <Route path="/createpost" element={<CreatePost />}></Route>
-        <Route path="login" element={<Login />}></Route>
-        <Route path="/logout" element={<Logout />}></Route>
+        <Route path="/login" element={<Login setisAuth={setisAuth} />}></Route>
+        <Route
+          path="/logout"
+          element={<Logout setisAuth={setisAuth} />}
+        ></Route>
       </Routes>
     </Router>
   );
