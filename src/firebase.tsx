d14446eaff3370b9dc8f84ca2fa1badaadd05079
sh-kitazawa <sh-kitazawa@kentem.co.jp>
2025-06-06 15:28:43 +0900

    TypeScriptに変更

---
+++ src/firebase.tsx
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCJAPkWbBSfdmgLxn06flOqqFUnIjH_Ig4",
+  authDomain: "blog-a182f.firebaseapp.com",
+  projectId: "blog-a182f",
+  storageBucket: "blog-a182f.firebasestorage.app",
+  messagingSenderId: "799630764798",
+  appId: "1:799630764798:web:d116b20398ad48bc9293af",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+provider.setCustomParameters({
+  prompt: "select_account",
+});
+const db = getFirestore(app);
+
+export { auth, provider, db };
