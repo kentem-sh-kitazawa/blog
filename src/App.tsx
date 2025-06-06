45a00e86c2b9e4997caf5801dff2ef8fc9a0d2ed
sh-kitazawa <sh-kitazawa@kentem.co.jp>
2025-06-06 12:59:32 +0900

    開発環境の構築とルーティング設定

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./components/Home";
+import CreatePost from "./components/CreatePost";
+import Login from "./components/Login";
+import Logout from "./components/Logout";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        {/* URLの最後にpathを付け足すと行きたいコンポーネントを指定できる*/}
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/createpost" element={<CreatePost />}></Route>
+        <Route path="login" element={<Login />}></Route>
+        <Route path="/logout" element={<Logout />}></Route>
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
