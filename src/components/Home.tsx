719cb090a1d22d6cc740d61e6235e43b543c06b6
sh-kitazawa <sh-kitazawa@kentem.co.jp>
2025-06-06 17:04:26 +0900

    ホーム画面にHTML要素を仮置き

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <h2>仮タイトル</h2>
+      <p>仮本文</p>
+      <h3>＠名前</h3>
+    </div>
+  );
 };
 
 export default Home;
