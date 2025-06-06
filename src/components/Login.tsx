b1601bbdb1ed8821ca68568a518fd8df3c8128ae
sh-kitazawa <sh-kitazawa@kentem.co.jp>
2025-06-06 15:30:32 +0900

    ログイン機能を実装

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,7 +1,29 @@
+import { signInWithPopup } from "firebase/auth";
 import React from "react";
+import { auth, provider } from "../firebase.tsx";
+import { useNavigate } from "react-router-dom";
 
-const Login = () => {
-  return <div>Login</div>;
+type Props = {
+  setisAuth: any;
+};
+
+const Login = ({ setisAuth }: Props) => {
+  const navigate = useNavigate();
+  const loginInWithGoogle = () => {
+    //Googleでログイン
+    signInWithPopup(auth, provider).then((result) => {
+      localStorage.setItem("isAuth", "true"); //ローカルストレージには文字列として保存されるため
+      setisAuth(true);
+      navigate("/");
+    });
+  };
+
+  return (
+    <div>
+      <p>ログインして始める</p>
+      <button onClick={loginInWithGoogle}>Googleでログイン</button>
+    </div>
+  );
 };
 
 export default Login;
