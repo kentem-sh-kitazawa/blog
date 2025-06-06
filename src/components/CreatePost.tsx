108ba414e42f0e650534437ef486333cf7c3d34a
sh-kitazawa <sh-kitazawa@kentem.co.jp>
2025-06-06 16:00:53 +0900

    記事投稿ページを作成

--- src/components/CreatePost.tsx
+++ src/components/CreatePost.tsx
@@ -1,7 +1,32 @@
-import React from "react";
-
+import { useState } from "react";
 const CreatePost = () => {
-  return <div>CreatePost</div>;
+  const [title, setTitle] = useState<string>("");
+  const [mainText, setMainText] = useState<string>("");
+  return (
+    <div>
+      <h2>記事を投稿する</h2>
+      <label>
+        タイトル
+        <input
+          type="text"
+          value={title}
+          onChange={(titleText) => {
+            setTitle(titleText.target.value);
+          }}
+        ></input>
+      </label>
+      <label>
+        投稿
+        <textarea
+          value={mainText}
+          onChange={(maintext) => {
+            setMainText(maintext.target.value);
+          }}
+        ></textarea>
+      </label>
+      <button>投稿する</button>
+    </div>
+  );
 };
 
 export default CreatePost;
