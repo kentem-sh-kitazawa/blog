3639165d5f6612f9cc2a79b1feb7dfe6b2fb5359
sh-kitazawa <sh-kitazawa@kentem.co.jp>
2025-06-09 09:11:06 +0900

    ボタンを押したときに作成した記事を保存する処理を追加

--- src/components/CreatePost.tsx
+++ src/components/CreatePost.tsx
@@ -1,7 +1,14 @@
 import { useState } from "react";
 import "../style/CreatePost.css";
+
+export type PostTextType = {
+  title: string;
+  mainText: string;
+};
 const CreatePost = () => {
+  //titleを保持するstate
   const [title, setTitle] = useState<string>("");
+  //mainTextを保持するstate
   const [mainText, setMainText] = useState<string>("");
   return (
     <section>
@@ -24,7 +31,20 @@ const CreatePost = () => {
             setMainText(maintext.target.value);
           }}
         ></textarea>
-        <button className="createPostButton">投稿する</button>
+        <button
+          className="createPostButton"
+          onClick={() => {
+            const posts: PostTextType = { title: title, mainText: mainText };
+            const prevPosts = localStorage.getItem("posts");
+            const parsePosts = prevPosts ? JSON.parse(prevPosts) : [];
+            const updatePosts = [...parsePosts, posts];
+            localStorage.setItem("posts", JSON.stringify(updatePosts));
+            setTitle("");
+            setMainText("");
+          }}
+        >
+          投稿する
+        </button>
       </div>
     </section>
   );
