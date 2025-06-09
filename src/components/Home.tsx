dc39c8b6cfc3ab2a1fe491bcfdeb5eb9503c8406
sh-kitazawa <sh-kitazawa@kentem.co.jp>
2025-06-09 09:11:51 +0900

    記事の一覧を表示する処理を追加

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,11 +1,41 @@
-import React from "react";
-
+import type { PostTextType } from "./CreatePost";
+import { useEffect, useState } from "react";
+import "../style/Home.css";
 const Home = () => {
+  const [posts, setPosts] = useState<PostTextType[]>([]);
+
+  useEffect(() => {
+    const savePosts = localStorage.getItem("posts");
+    const parsedPosts: PostTextType[] = savePosts ? JSON.parse(savePosts) : [];
+    setPosts(parsedPosts);
+  }, []);
+
   return (
-    <div>
-      <h2>仮タイトル</h2>
-      <p>仮本文</p>
-      <h3>＠名前</h3>
+    <div className="posts">
+      <ul>
+        {posts.map((post, index) => (
+          <li key={index}>
+            <h2>{post.title}</h2>
+            <p>{post.mainText}</p>
+            <div className="footer">
+              <h3>＠名前</h3>
+              <button
+                className="postDeleteButton"
+                onClick={() => {
+                  setPosts((prev) => {
+                    const updatePosts = [...prev];
+                    updatePosts.splice(index, 1);
+                    localStorage.setItem("posts", JSON.stringify(updatePosts));
+                    return updatePosts;
+                  });
+                }}
+              >
+                削除
+              </button>
+            </div>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
