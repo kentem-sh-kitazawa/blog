b6b013b79567e490841ea243b835e086a7aedf52
sh-kitazawa <sh-kitazawa@kentem.co.jp>
2025-06-09 10:20:42 +0900

    HomeにHTML要素を新しく配置、CSS用にクラスを定義

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -4,39 +4,54 @@ import "../style/Home.css";
 const Home = () => {
   const [posts, setPosts] = useState<PostTextType[]>([]);
 
-  useEffect(() => {
-    const savePosts = localStorage.getItem("posts");
-    const parsedPosts: PostTextType[] = savePosts ? JSON.parse(savePosts) : [];
-    setPosts(parsedPosts);
-  }, []);
+  // useEffect(() => {
+  //   const savePosts = localStorage.getItem("posts");
+  //   const parsedPosts: PostTextType[] = savePosts ? JSON.parse(savePosts) : [];
+  //   setPosts(parsedPosts);
+  // }, []);
 
   return (
-    <div className="posts">
-      <ul>
-        {posts.map((post, index) => (
-          <li key={index}>
-            <h2>{post.title}</h2>
-            <p>{post.mainText}</p>
-            <div className="footer">
-              <h3>＠名前</h3>
-              <button
-                className="postDeleteButton"
-                onClick={() => {
-                  setPosts((prev) => {
-                    const updatePosts = [...prev];
-                    updatePosts.splice(index, 1);
-                    localStorage.setItem("posts", JSON.stringify(updatePosts));
-                    return updatePosts;
-                  });
-                }}
-              >
-                削除
-              </button>
-            </div>
-          </li>
-        ))}
-      </ul>
+    <div className="homePage">
+      <div className="postContents">
+        <div className="postHeder">
+          <h1>タイトル</h1>
+        </div>
+        <div className="postTextContainer">
+          aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+        </div>
+        <div className="nameAndDeleteButton">
+          <h3>@a</h3>
+          <button>削除</button>
+        </div>
+      </div>
     </div>
+    //ローカルストレージ版
+    // <div className="posts">
+    //   <ul>
+    //     {posts.map((post, index) => (
+    //       <li key={index}>
+    //         <h2>{post.title}</h2>
+    //         <p>{post.mainText}</p>
+    //         <div className="footer">
+    //           <h3>＠名前</h3>
+    //           <button
+    //             className="postDeleteButton"
+    //             onClick={() => {
+    //               setPosts((prev) => {
+    //                 const updatePosts = [...prev];
+    //                 updatePosts.splice(index, 1);
+    //                 localStorage.setItem("posts", JSON.stringify(updatePosts));
+    //                 return updatePosts;
+    //               });
+    //             }}
+    //           >
+    //             削除
+    //           </button>
+    //         </div>
+    //       </li>
+    //     ))}
+    //   </ul>
+    // </div>
   );
 };
 
