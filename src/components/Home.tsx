01bc63b1f919db59cbecc9700ff166aa858cb77d
sh-kitazawa <sh-kitazawa@kentem.co.jp>
2025-06-09 11:09:28 +0900

    記事の一覧をfirebaseから取得して表示する処理を追加

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,8 +1,18 @@
-import type { PostTextType } from "./CreatePost";
+// import type { PostTextType } from "./CreatePost";
 import { useEffect, useState } from "react";
 import "../style/Home.css";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../firebase";
 const Home = () => {
-  const [posts, setPosts] = useState<PostTextType[]>([]);
+  // const [posts, setPosts] = useState<PostTextType[]>([]);
+  const [posts, setPosts] = useState<any[]>([]);
+
+  type PostsType = {
+    author: { id: string; username: string };
+    id: string;
+    mainText: string;
+    title: string;
+  };
 
   // useEffect(() => {
   //   const savePosts = localStorage.getItem("posts");
@@ -10,20 +20,33 @@ const Home = () => {
   //   setPosts(parsedPosts);
   // }, []);
 
+  useEffect(() => {
+    const getPosts = async () => {
+      const data = await getDocs(collection(db, "posts"));
+      setPosts(
+        data.docs.map((doc) => ({
+          ...doc.data(),
+          id: doc.id,
+        }))
+      );
+    };
+    getPosts();
+  }, []);
+
   return (
     <div className="homePage">
-      <div className="postContents">
-        <div className="postHeder">
-          <h1>タイトル</h1>
+      {posts.map((post) => (
+        <div className="postContents" key={post.id}>
+          <div className="postHeder">
+            <h1>{post.title}</h1>
+          </div>
+          <div className="postTextContainer">{post.mainText}</div>
+          <div className="nameAndDeleteButton">
+            <h3>{post.author.username}</h3>
+            <button>削除</button>
+          </div>
         </div>
-        <div className="postTextContainer">
-          aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-        </div>
-        <div className="nameAndDeleteButton">
-          <h3>@a</h3>
-          <button>削除</button>
-        </div>
-      </div>
+      ))}
     </div>
     //ローカルストレージ版
     // <div className="posts">
