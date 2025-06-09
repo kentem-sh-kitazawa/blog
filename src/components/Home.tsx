5fddb5505917309c89e3a1967997dc74143cb8b6
sh-kitazawa <sh-kitazawa@kentem.co.jp>
2025-06-09 16:22:50 +0900

    クラス名を変更、不要な{}を削除

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -5,12 +5,12 @@ import { auth, db } from "../firebase";
 
 import "../style/Home.css";
 
-  type PostsType = {
-    author: { username: string; id: string };
-    id: string;
-    mainText: string;
-    title: string;
-  };
+type PostsType = {
+  author: { username: string; id: string };
+  id: string;
+  mainText: string;
+  title: string;
+};
 
 const Home = () => {
   const [posts, setPosts] = useState<any[]>([]);
@@ -34,24 +34,16 @@ const Home = () => {
   };
 
   return (
-    <div className="homePage">
+    <div className="home-page">
       {posts.map((post: PostsType) => (
-        <div className="postContents" key={post.id}>
-          <div className="postHeder">
-            <h1>{post.title}</h1>
-          </div>
-          <div className="postTextContainer">{post.mainText}</div>
-          <div className="nameAndDeleteButton">
+        <div className="post-contents" key={post.id}>
+          <h1 className="post-heder">{post.title}</h1>
+          <div className="post-text-container">{post.mainText}</div>
+          <div className="post-fotter">
             <h3>{post.author.username}</h3>
             {/* ポストのidと現在ログインしているユーザーのid */}
             {post.author.id === auth.currentUser?.uid && (
-              <button
-                onClick={() => {
-                  handleDelete(post.id);
-                }}
-              >
-                削除
-              </button>
+              <button onClick={() => handleDelete(post.id)}>削除</button>
             )}
           </div>
         </div>
